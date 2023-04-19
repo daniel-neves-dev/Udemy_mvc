e71b1ef50715559d2259536c75dcd3806ee29456
daniel <daniel.oliveira.au@hotmail.com>
2023-04-18 21:01:25 -0300

    feat: user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true,
             uniqueness: {case_sensitive: false},
-            length: {maximum:105}
+            length: {maximum:105},
+            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
 end
