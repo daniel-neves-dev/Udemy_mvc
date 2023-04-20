a08475efda57808a457bbeac24d42557c9724434
daniel <daniel.oliveira.au@hotmail.com>
2023-04-20 20:21:24 -0300

    feat: user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   before_save {self.email = email.downcase}
   has_many :articles
+  has_secure_password
 
   validates :name, presence: true,
             uniqueness: {case_sensitive: false},
