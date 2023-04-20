05ee4fd94dc3c3981b668e5baec83d408f1dc3d9
daniel <daniel.oliveira.au@hotmail.com>
2023-04-20 19:57:20 -0300

    feat: user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :articles
+
   validates :name, presence: true,
             uniqueness: {case_sensitive: false},
             length: {minimum:3, maximum:25}
