37562809695e1041bb69c3aefb5ba646f6578ec2
daniel <daniel.oliveira.au@hotmail.com>
2023-04-22 10:48:31 -0300

    feat: user pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 2)
+
   end
   def show
     @user = User.find(params[:id])
