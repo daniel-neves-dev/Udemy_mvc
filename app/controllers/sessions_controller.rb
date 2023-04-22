a07c81b8b41f188220dca23128c3285f9dbf8c0b
daniel <daniel.oliveira.au@hotmail.com>
2023-04-22 11:57:39 -0300

    feat: user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,18 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash[:alert] = "Information not correct"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-
   end
 end
