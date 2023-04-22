c19496709c55ff6f6228df48224f4498f58b7166
daniel <daniel.oliveira.au@hotmail.com>
2023-04-22 13:02:29 -0300

    feat: user login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog #{@user.name}"
       redirect_to articles_path
     else
